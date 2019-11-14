2a6eab424b24a25e3c4546c58b1057bf8b531bad
WeiquanLuo <weiquan.luo1221@gmail.com>
2019-11-14 15:24:29 -0600

    m

--- R/addcoord.R
+++ R/addcoord.R
@@ -3,7 +3,7 @@
 
 addcoord <- function(site_sf){
   # add COORDS in projected form (UTM)
-  site_sf$X <- sf::st_coordinates(site_sf)[,1]
-  site_sf$Y <- sf::st_coordinates(site_sf)[,2]
+  site_sf$X <- st_coordinates(site_sf)[,1]
+  site_sf$Y <- st_coordinates(site_sf)[,2]
   return(site_sf)
 }
