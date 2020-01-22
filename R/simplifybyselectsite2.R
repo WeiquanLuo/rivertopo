0feaed414e68fd2ecc36d08102572eed8662ff66
WeiquanLuo <weiquan.luo1221@gmail.com>
2020-01-21 23:13:00 -0600

    not export the slower simplify2.r

--- R/simplifybyselectsite2.R
+++ R/simplifybyselectsite2.R
@@ -5,7 +5,6 @@
 #' @importFrom raster crs
 #' @importFrom stats setNames
 #' @importFrom xts last
-#' @export
 #' @param site_sf a site sf object with column: id, lon, lat, geometry, X, Y, and the snapped point information from the riverdist::xy2segvert() columns: seg, vert, snapdist
 #' @param arc_sf.set a arc sf object from makearc() containing columns:
 #' from, seg0, vert0, x0, y0, to, seg1, vert1, x1, y1, geometry
