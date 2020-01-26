d2d28bdf7a83b683d9a1965a4985e45e56ba18e9
WeiquanLuo <weiquan.luo1221@gmail.com>
2020-01-26 15:37:20 -0600

    change site2sf, lon to longitude. lat to lagitue

--- R/site2sf.R
+++ R/site2sf.R
@@ -11,14 +11,13 @@
 #' the surface of the earth or a portion of the earth on a flat piece of paper or
 #' computer screen.
 #'
-site2sf <- function(df,id_cn, Lon_cn, Lat_cn, crs= 4326){
+site2sf <- function(df,id_cn ="id", Lon_cn = "longitude", Lat_cn = "latitude", crs= 4326){
   # cleanup data
-  df_locs <- df %>% rename(lon=Lon_cn, lat=Lat_cn, id = id_cn)
+  df_locs <- df %>% rename(longitude=Lon_cn, latitude=Lat_cn, id = id_cn)
   # convert to sf object
   df_locs <- st_as_sf(df_locs,
-                      coords = c("lon", "lat"), # for point data
+                      coords = c("longitude", "latitude"), # for point data
                       remove = F, # don't remove these lat/lon cols from df
                       crs = crs) # add projection (this is WGS84)
-  df_locs <- df_locs %>% select(id, lon, lat, geometry)
   return(df_locs)
 }
